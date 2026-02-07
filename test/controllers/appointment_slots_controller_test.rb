41dd3a8b5c1ebe5495f2c8a95b608a216fa94388
T-ASWIN <aswinmani53@gmail.com>
2026-02-07 04:46:11 +0000

    Fix appointment slots test route helper

--- test/controllers/appointment_slots_controller_test.rb
+++ test/controllers/appointment_slots_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class AppointmentSlotsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get appointment_slots_index_url
+    get appointment_slots_url
     assert_response :success
   end
 end
